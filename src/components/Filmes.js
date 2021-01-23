fdc960905c8582ab6f75a581cd28c163d6946ebb
rafael-sodre <rafael.sodre@me.com>
2021-01-23 16:34:59 -0300

    Buscando filmes na API. Aplicando estilos. Separando componentes

--- src/components/Filmes.js
+++ src/components/Filmes.js
@@ -1,14 +1,18 @@
-import Filme from './Filme'
-
-const Filmes = ({ filmes }) => {
-    return (
-        <>
-        {filmes.map((filme) => (
-            <Filme key={filme.imdbID} filme={filme}/>
-        ))}
-        </>
+const Filmes = ({filmes}) => {
+    return(
+        <div className="movie-container">
+            {filmes.map(filme => (
+                <div key={filme.imdbID} className="movie">
+                    <img src={filme.Poster}/>
+                    <div className="movie-info">
+                        <h3>{filme.Title}</h3>
+                        <p>{filme.Year}</p>
+                    </div>
+                    
+                </div>
+            ))}
+        </div>
     )
 }
-
 
 export default Filmes
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,15 @@
-const Header = () => {
-    return (
+const Header = ({ handleOnSubmit, termoBusca, handleOnChange }) => {
+    return(
         <header>
-            <h1>OMDB Movies</h1>
+            <h1>OMDB API React</h1>
+            <form onSubmit={handleOnSubmit}>
+                <input 
+                type='Search'
+                placeholder='Buscar'
+                value={termoBusca}
+                onChange={handleOnChange}
+                />
+            </form>
         </header>
     )
 }
